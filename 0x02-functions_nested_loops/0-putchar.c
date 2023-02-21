3cfd8b27234ada63f61a3c36331b51d9b26ca0b9
Lincoln Langat <lincolnlangat@gmail.com>
2023-02-21 11:42:30 -0800

    c func

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Print "putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char p[] = "putchar";
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		_putchar(p[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
